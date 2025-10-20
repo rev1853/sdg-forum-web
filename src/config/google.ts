939a430505efb0f5427e79890be743522e2c1ce2
rev1853 <rghofur053@gmail.com>
2025-10-20 10:59:00 +0700

    done

--- src/config/google.ts
+++
@@ -1,4 +0,0 @@
-export const GOOGLE_OAUTH_CONFIG = Object.freeze({
-    clientId: '199413225799-74tms0ordfb6vnulfs7tthopumesec4a.apps.googleusercontent.com',
-    clientSecret: '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',
-});
